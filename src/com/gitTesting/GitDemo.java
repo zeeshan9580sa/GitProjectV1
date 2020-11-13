bdaa93ce70422175fbded9d9bd04c14500a470c6
Zeeshan ali <zeeshanali9580@gmail.com>
2020-11-13 09:13:20 +0530

    Added if else condition

--- src/com/gitTesting/GitDemo.java
+++ src/com/gitTesting/GitDemo.java
@@ -3,8 +3,17 @@ package com.gitTesting;
 public class GitDemo {
 
 	public static void main(String[] args) {
-System.out.println("Welcome to Git Tutorial");
-System.out.println("Yes, iahve make clone the repo");
+		System.out.println("Welcome to Git Tutorial");
+		System.out.println("Yes, iahve make clone the repo");
+		String name="Zeeshan";
+		if(name=="Zeeshan")
+		{
+			System.out.println("Welcome Zeeshan");
+		}
+		else
+			System.out.println("***************");
 	}
 
 }
+
+
